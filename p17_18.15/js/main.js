58dc4633be2734ff6b9b48e757e12c78ba587fd2
shizhongzheng <chengxiaszz@sina.com>
2018-12-20 00:17:07 +0800

    【add】directive

--- p17_18.15/js/main.js
+++ p17_18.15/js/main.js
@@ -1,4 +1,10 @@
-
+/**
+ * 函数简写
+ * 在很多时候，你可能想在 bind 和 update 时触发相同行为，而不关心其它的钩子。比如这样写:
+ * el: 指令所绑定的元素，可以用来直接操作DOM
+ * binding: 一个对象，包含以下属性：name, value, oldValue, expression, arg, modifiers
+ * 注意：除了el之外，其他参数应该都是只读的，切勿进行修改。
+ */
 
 Vue.directive('pin', function(el, binding) {
 
